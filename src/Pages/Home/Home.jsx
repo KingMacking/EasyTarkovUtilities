6da25a8b395258f8469f18cdac46009dceb72ad0
KingMacking <matiasatzoridev@gmail.com>
2024-02-02 04:17:51 -0300

    FIX: Mobile view and styles

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import ListItem from '../../components/ListItem/ListItem'
 
 const Home = () => {
     return (
-        <main className="flex flex-col items-center justify-center w-full h-full bg-black ">
+        <main className="flex flex-col items-center justify-center w-full min-h-screen bg-black ">
             <h1 className="mb-6 text-6xl font-bold font-primary text-primary">Easy Tarkov Utilities</h1>
             <ul className="flex flex-wrap items-center justify-center gap-4 md:max-w-5xl lg:max-w-7xl">
                 <ListItem imgAlt={"Map of tarkov"} imgSrc={"/tarkov-map.webp"} name={"Maps"} linkTo={"maps"}/>
